cace023799875c9d1a887805ebc0c1322bef876d
erickahr86 <erickahr86@gmail.com>
2021-09-05 22:23:16 -0500

    Extension Functions

--- app/src/main/java/com/example/myapplication/activities/ExtensionFunctionActivity.kt
+++ app/src/main/java/com/example/myapplication/activities/ExtensionFunctionActivity.kt
@@ -2,11 +2,32 @@ package com.example.myapplication.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myapplication.MainActivity
 import com.example.myapplication.R
+import com.example.myapplication.databinding.ActivityExtensionFunctionBinding
+import com.example.myapplication.others.goToActivity
+import com.example.myapplication.others.loadByURL
+import com.example.myapplication.others.snackBar
+import com.example.myapplication.others.toast
 
 class ExtensionFunctionActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityExtensionFunctionBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_extension_function)
+
+        binding = ActivityExtensionFunctionBinding.inflate( layoutInflater )
+        setContentView( binding.root )
+
+        binding.buttonToast       .setOnClickListener { toast("Hello") }
+        binding.buttonSnackBar    .setOnClickListener {
+            snackBar(
+                "Hello from SnackBar!",
+                action      = "Done",
+                actionEvent = { toast("Cool!") }) }
+        binding.buttonLoadByUrl   .setOnClickListener { binding.imageLoadedByUrl.loadByURL("http://images.foody.vn/res/g14/138986/prof/s576x330/foody-mobile-a2-jpg-261-635682356468932282.jpg") }
+        binding.buttonGoToActivity.setOnClickListener { goToActivity<MainActivity>{ putExtra("id", 1) } }
+
     }
 }
